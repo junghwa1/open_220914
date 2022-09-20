cd6b72933c5cd5681b4ba1d05f870394d31e470f
junghwa1 <57510050+junghwa1@users.noreply.github.com>
2022-09-20 19:54:58 +0900

    Add files via upload

---
+++ CircleArea_2.java
@@ -0,0 +1,11 @@
+package opensource2;
+
+public class CircleArea_2 {
+
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 10.0;
+        double circleArea = radius * radius * PI;
+        System.out.println("원의 면적 = " + circleArea);
+    }
+}
---
+++ MultipleOfThree_11.java
@@ -0,0 +1,17 @@
+package opensource2;
+
+import java.util.Scanner;
+public class MultipleOfThree_11 {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("수를 입력하시오: ");
+        int number = in.nextInt();
+
+        if (number % 3 == 0)
+            System.out.println("3의 배수입니다.");
+        else
+            System.out.println("3의 배수가 아닙니다.");
+
+        in.close();
+    }
+}
